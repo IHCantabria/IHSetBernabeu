6f197f09ae5200bc12f045dc7a9a628745382927
CB-Lim <129813815+CB-Lim@users.noreply.github.com>
2024-03-26 11:14:00 +0100

    Add files via upload

--- src/IHSetBernabeu/IHSetBernabeu.py
+++ src/IHSetBernabeu/IHSetBernabeu.py
@@ -1,6 +1,62 @@
 import numpy as np
 from scipy.interpolate import interp1d
 from shapely.geometry import LineString
+import xarray as xr
+
+class cal_Bernabeu(object):
+    """
+    cal_Bernabeu
+    
+    Configuration to calibrate and run the Bernabeu profile.
+    
+    This class reads input datasets, performs its calibration.
+    """
+    def __init__(self, path):
+        self.path = path
+        
+        # cfg = xr.open_dataset(path+'config.nc')
+        ens = xr.open_dataset(path+'ens.nc')
+        wav = xr.open_dataset(path+'wav.nc')
+                
+        # self.Ymin = cfg['Ymin'].values
+        # self.Ymax = cfg['Ymax'].values
+        # self.dY = cfg['dy'].values
+        self.D50 = ens['D50'].values
+        self.dp = ens['d'].values
+        self.zp = ens['z'].values
+        self.CM = ens['CM_95'].values
+        self.Hs = wav['Hs50'].values
+        self.Tp = wav['Tp50'].values
+                
+    def calibrate(self):
+        self.zp = self.zp - self.zp[0]
+        self.dd = self.dp - self.dp[0]
+
+        # Profile with equidistant points
+        dp = np.linspace(0, self.dp[-1], 500).reshape(-1, 1)
+        
+        interp_func = interp1d(self.dd, self.zp, kind="linear", fill_value="extrapolate")
+        zp = interp_func(dp)
+        zp = zp[1:]
+        dp = dp[1:]
+        
+        ws = caida_grano(self.D50)
+        gamma = self.Hs / (ws * self.Tp)
+    
+        self.Ar = 0.21 - 0.02 * gamma
+        self.B = 0.89 * np.exp(-1.24 * gamma)
+        self.C = 0.06 + 0.04 * gamma
+        self.D = 0.22 * np.exp(-0.83 * gamma)    
+
+        self.a = self.Ar**(-1.5)
+        self.b = self.B / self.Ar**(1.5)
+        self.c = self.C**(-1.5)
+        self.d = self.D / self.C**(1.5)
+        
+        self.ha = 3 * self.Hs
+        self.hr = 1.1 * self.Hs
+        
+        return self       
 
 def caida_grano(D50):
     ws = np.nan
@@ -17,49 +73,21 @@ def RMSEq(Y, Y2t):
     return np.sqrt(np.mean((Y - Y2t) ** 2, axis=0))
 
 
-def Bernabeu(dp, zp, CM, D50, Hs, Tp):
-    z = zp - zp[0]
-    d = dp - dp[0]
-    
-    # Profile with equidistant points
-    dp = np.linspace(0, dp[-1], 500).reshape(-1, 1)  # 500 points
-    interp_func = interp1d(d, z, kind='linear', fill_value='extrapolate')
-    zp = interp_func(dp)
-    zp = zp[1:]
-    dp = dp[1:]
-    
-    rotura = 0
-    ws = caida_grano(D50)
-    gamma = Hs / (ws * Tp)
-    
-    Ar = 0.21 - 0.02 * gamma
-    B = 0.89 * np.exp(-1.24 * gamma)
-    C = 0.06 + 0.04 * gamma
-    D = 0.22 * np.exp(-0.83 * gamma)    
-
-    Ks = [Ar, B, C, D]
-
-    a = Ar**(-1.5)
-    b = B / Ar**(1.5)
-    c = C**(-1.5)
-    d = D / C**(1.5)
-
-    ha = 3 * Hs
-    hr = 1.1 * Hs
-
-    Xr = ((hr + CM) / Ar)**(1.5) + B / Ar**(1.5) * (hr + CM)**3
-    Xo = Xr - (hr / C)**(1.5) - D / C**(1.5) * hr**3
-    Xa = Xo + c * (ha**(1.5)) + d * ha**3
+def Bernabeu(self):
+    Xr = ((self.hr + self.CM) / self.Ar)**(1.5) + self.B / self.Ar**(1.5) * (self.hr + self.CM)**3
+    Xo = Xr - (self.hr / self.C)**(1.5) - self.D / self.C**(1.5) * self.hr**3
+    Xa = Xo + self.c * (self.ha**(1.5)) + self.d * self.ha**3
 
     hini = np.arange(0, 10.1, 0.1)
-    xini = a * hini**1.5 + b * hini**3
+    xini = self.a * hini**1.5 + self.b * hini**3
 
+    rotura = 0
     hrot = rotura - hini
     xrot = xini + rotura
 
-    xdos = c * hini**(1.5) + d * hini**3
+    xdos = self.c * hini**(1.5) + self.d * hini**3
 
-    haso = rotura - hini - CM
+    haso = rotura - hini - self.CM
     xaso = xdos + Xo + rotura
 
     line_coords1 = list(zip(xaso, haso))
@@ -80,21 +108,8 @@ def Bernabeu(dp, zp, CM, D50, Hs, Tp):
     X = np.concatenate([xrot, xaso])
     hm = np.concatenate([hrot, haso])
 
-    hmi = interp1d(X, hm, kind='linear', fill_value='extrapolate')(dp)  
+    self.hmi = interp1d(X, hm, kind='linear', fill_value='extrapolate')(self.dp)  
 
-    err = RMSEq(zp, hmi)
+    err = RMSEq(self.zp, self.hmi)
 
-    Para = {
-        'model': 'Bernabeu (Dos tramos)',
-        'formulation': [
-            r'$x_r = \frac{h}{A_r}^{1.5} + \frac{B}{A_r^{1.5}}h^3$',
-            r'$x_a = x - x_0 = \left(\frac{h-M}{C}\right)^{1.5} + \frac{D}{C^{1.5}}(h-M)^3$'
-        ],
-        'name_coeffs': ['Ar', 'B', 'C', 'D'],
-        'coeffs': Ks,
-        'RMSE': err
-    }
-    
-    model = {'D': np.concatenate([[0], dp.flatten()]), 'Z': np.concatenate([[0], hmi.flatten()])}
-
-    return Para, model          
+    return self  
--- src/IHSetBernabeu/tests/test_bernabeu.py
+++ src/IHSetBernabeu/tests/test_bernabeu.py
@@ -1,25 +1,19 @@
-from scipy import io
-import numpy as np
 from IHSetBernabeu import IHSetBernabeu
-from scipy.interpolate import interp1d
+import os
+import matplotlib.pyplot as plt
 
-def test_Bernabeu():
+wrkDir = os.getcwd()
+model = IHSetBernabeu.cal_Bernabeu(wrkDir+'/data/')
+self = IHSetBernabeu.Bernabeu(model.calibrate())
 
-    perfil = io.loadmat("./data/perfiles_cierre.mat")
-    p = 0
-    d = perfil["perfil"]["d"][0][p].flatten()
-    d = d - d[0]
-    z = perfil["perfil"]["z"][0][p].flatten()
-    CM = perfil["perfil"]["CM_95"][0][p].flatten()
-    Hs = perfil["perfil"]['Hs50'][0][p].flatten()
-    Tp = perfil["perfil"]['Tp50'][0][p].flatten()    
-    z = z - CM
-    di = np.linspace(d[0], d[-1], 100)
-    z = interp1d(d, z, kind="linear", fill_value="extrapolate")(di)
-    d = di
+plt.rcParams.update({'font.family': 'serif'})
+plt.rcParams.update({'font.size': 7})
+plt.rcParams.update({'font.weight': 'bold'})
+font = {'family': 'serif',
+        'weight': 'bold',
+        'size': 8}
 
-    D50 = perfil["perfil"]["D50"][0][p].flatten()
-
-    # Assuming the 'ajuste_perfil' function is defined as in the previous code
-    pBerna, mBerna = IHSetBernabeu.Bernabeu(d, z, CM, D50, Hs, Tp)
-    assert pBerna["RMSE"][0] == 2.9464755964823985
+hk = []
+hk.append(plt.plot(self.dp, self.zp, '--k')[0])
+hk.append(plt.plot(self.dp, self.hmi, linewidth=2)[0])
+plt.show()
